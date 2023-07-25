1c9d6a90dd6e8984f5fce684dbcd0b297dcb3708
Ajoy24 <annika.joy@icloud.com>
2023-07-25 14:28:33 -0700

    Update Expressions Project.java

--- Expressions Project.java
+++ Expressions Project.java
@@ -36,30 +36,31 @@ public class Main {
         System.out.println(((8 - 5) + Math.pow(3,4)/5));
 
         System.out.println(3+Math.pow(3,4)/5);
-        System.out.println(3+81/5);
-        System.out.println(3+16);
-        System.out.println(19);
+        System.out.println(3+81.0/5);
+        System.out.println(3+16.2);
+        System.out.println(19.2);
 
         //Expression 4
         System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
 
         System.out.println(1-8+Math.pow(2,3)/8+Math.sqrt(81)/3);
-        System.out.println(1-8+8/8+Math.sqrt(81)/3);
-        System.out.println(1-8+8/8+9/3);
-        System.out.println(1-8+1+9/3);
-        System.out.println(1-8+1+3);
-        System.out.println(-7+1+3);
-        System.out.println(-6+3);
-        System.out.println(-3);
+        System.out.println(1-8+8.0/8+Math.sqrt(81)/3);
+        System.out.println(1-8+8.0/8+9.0/3);
+        System.out.println(1-8+1.0+9.0/3);
+        System.out.println(1-8+1.0+3.0);
+        System.out.println(-7+1.0+3.0);
+        System.out.println(-6.0+3.0);
+        System.out.println(-3.0);
 
         //Expression 5
         System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        
         System.out.println(((7+3)*Math.sin(45)+2)-Math.pow(Math.sqrt(25),3));
-        System.out.println((10*Math.sin(45)+2)-Math.pow(5,3));
-        System.out.println((10*0.9+2)-Math.pow(5,3));
-        System.out.println((9+2)-Math.pow(5,3));
-        System.out.println(11-Math.pow(5,3));
-        System.out.println(11-125);
-        System.out.println(-114);
+        System.out.println((10*Math.sin(45)+2)-Math.pow(5.0,3));
+        System.out.println((10*0.85+2)-Math.pow(5.0,3));
+        System.out.println((8.5+2)-Math.pow(5.0,3));
+        System.out.println(10.5-Math.pow(5.0,3));
+        System.out.println(10.5-125.0);
+        System.out.println(-114.5);
     }
 }
